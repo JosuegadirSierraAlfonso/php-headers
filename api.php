a293a81db93c0fbc93369dd0d49f669ed01c7ee2
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-18 08:28:31 -0500

    feat: :sparkles: Headers PHP
    the headers are obtained

---
+++ api.php
@@ -0,0 +1,7 @@
+<?php
+    header("Content-Type: application/json");
+    $_DATA = json_decode(file_get_contents("php//input"), true);
+    $_METHOD = $_SERVER["REQUEST_METHOD"];
+    $_HEADERS = apache_request_headers();
+    var_dump($_HEADERS);
+?>
